e5b3c1ddd06f9ca91acd60d18504251cd4b8b66f
Atik2788 <ashanto653.2@gmail.com>
2025-05-01 00:36:53 +0600

    starting 1.6

---
+++ module1/src/1.5.ts
@@ -0,0 +1,13 @@
+// Reference Type
+
+const user: {
+    company: "Programming Hero"; // --> literal types
+    firstName: string;
+    middleName?: string; // --> optional type
+    lastName: string;
+} = {
+    company: 'Programming Hero',
+    firstName: 'Mejba',
+    lastName: "Pershian"
+}
+
