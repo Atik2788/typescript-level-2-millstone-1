0efa7af901dd80216e22e47e6bbb703eabc12545
Atik2788 <ashanto653.2@gmail.com>
2025-04-30 23:56:38 +0600

    starting 1.5

--- module1/src/1.4.ts
+++ module1/src/1.4.ts
@@ -10,3 +10,22 @@ let isAdmin: boolean = true;
 let x: undefined = undefined;
 // null
 let y: null= null;
+
+let d : number;
+
+// d = 'asdfwef'
+d = 12365544;
+
+
+// Array
+let friends: string[] = ['Sadman', "Rubel"]
+// friends.push(7)
+
+let rollList: number[] = [1, 2, 3, 6, 8]
+
+
+// tuple ---> array --> 
+let coordinates : [number, number] = [5, 6]
+
+let ageName: [number, string] = [50, 'Mr. J']
+let ageName2: [number, string, boolean] = [50, 'Mr. J', false]
