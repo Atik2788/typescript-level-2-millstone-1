95d634e8277455d2bb0cb5d6feabefa31df00457
Atik2788 <ashanto653.2@gmail.com>
2025-04-30 23:46:00 +0600

    first commit

---
+++ module1/src/1.4.ts
@@ -0,0 +1,12 @@
+// Basic Data Type
+
+// string
+let firstName: string = "Atik"
+// number
+let roll: number = 123;
+//boolean
+let isAdmin: boolean = true;
+// undefined
+let x: undefined = undefined;
+// null
+let y: null= null;
