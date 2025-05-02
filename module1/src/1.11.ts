d6b25d0aebc74a0a9b88ff55325d4e149255d705
Atik2788 <ashanto653.2@gmail.com>
2025-05-02 20:18:38 +0600

    1-11 Ternary, optional chaining & nullish coalescing operator

--- module1/src/1.11.ts
+++ module1/src/1.11.ts
@@ -1,7 +1,7 @@
 {
-    // Ternary Operator || Optional Chaining || Mullish Coalesching
+    // Ternary Operator****************
 
-    const age: number = 15;
+    const age: number = 25;
 
     if(age >=18){
         console.log('Aduld');
@@ -9,7 +9,41 @@
         console.log('Not Adult');
     }
 
+    const isAudult = age >= 18 ? "Adult" : "Not Adult"
 
 
+    // Nullish Coalesching Operator*****************
+    // null / undefine ---> decision making
+
+    const isAuthenticated1 = null;
+    // or
+    const isAuthenticated = undefined;
+
+    const result1 = isAuthenticated ?? "Guest";
+    console.log({result1});
+
+
+    // Optional Chaining
+    type User ={
+        name: string;
+        address:{
+            city: string;
+            road: string;
+            presendAddress: string;
+            permanentAddress?: string
+        }
+    }
+
+    const user: User = {
+        name: "Persian",
+        address:{
+            city: 'ctg',
+            road: "Awsome Road",
+            presendAddress: "ctg town "
+        }
+    }
+
+    const permanentAddress = user?.address?.permanentAddress ?? "No permanent address";
+    console.log({permanentAddress});
 
 }
