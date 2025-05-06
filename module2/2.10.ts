c5573bcd85e80466b3559a2ee0281f6d95f983d7
Atik2788 <ashanto653.2@gmail.com>
2025-05-06 22:43:18 +0600

    // 2-10: Mapped types

---
+++ module2/2.10.ts
@@ -0,0 +1,19 @@
+{
+    // 2-10: Mapped types
+
+    type AreaNumber = {
+        height: Number;
+        width: number;
+    }
+    
+
+    type AreaString<T> ={
+        [key in keyof T] : T[key];
+    }
+
+    const area1:  AreaString<{height: string; width: number}> = {
+        height: '100',
+        width: 50
+    }
+
+}
