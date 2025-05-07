4859c06139e7f5f707d7b7c8e49bc6a26f251089
Atik2788 <ashanto653.2@gmail.com>
2025-05-07 09:24:41 +0600

    OOP Inheritence:

---
+++ module3/3.2.ts
@@ -0,0 +1,43 @@
+{
+    // OOP Inheritence: its 
+
+    // parent class
+    class Parent{
+
+        constructor(public name:string, public age:number, public address: string){
+
+        }
+        getSleep(numOfHOurs: number){
+            console.log(`${this.name} will sleep ${numOfHOurs} hours`);
+        }
+    }
+
+
+    // class 1
+    class Student extends Parent {
+
+        constructor(name:string, age:number, address: string){
+            super(name, age, address)
+        }        
+    }
+
+    const student1 = new Student("Mr X", 27, 'Dhaka')
+    student1.getSleep(8)
+
+
+    // class 2
+    class Teacher extends Parent {
+
+        constructor(name: string, age: number, address: string, public designation: string){
+            super(name, age, address)
+        }
+
+       getClass(numOfClass: number){
+            console.log(`${this.name} will take ${numOfClass} classes`);
+        }
+    }
+    
+    const teacher1 = new Teacher("Mr T", 40, 'Dhaka', "Uganda")
+    teacher1.getClass(8)
+
+}
