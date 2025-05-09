8a25c0543a0c23713412187a26092d26428c360a
Atik2788 <ashanto653.2@gmail.com>
2025-05-09 23:33:01 +0600

    module 3.7.ts done

---
+++ module3/3.7.ts
@@ -0,0 +1,20 @@
+{
+    // 7 Statics in OOP
+    class Counter{
+        static count: number = 0
+        
+        static increment(){
+            return (Counter.count = Counter.count +1);
+        }
+
+        static deccrement(){
+            return (Counter.count = Counter.count - 1)
+        }
+
+    }
+
+    console.log(Counter.increment());
+    console.log(Counter.increment());
+
+
+}
